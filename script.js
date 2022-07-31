d61d495836985de5e9b9b542e6175bf45ed08c01
Trever <hercztakk@gmail.com>
2022-07-31 12:00:27 -0500

    Added basic layout of coding quiz and added questions

---
+++ script.js
@@ -0,0 +1,54 @@
+let timer = 10
+let wrongAnswerTime = 10
+let totalTime = 100
+let questions = [{
+    question: "Which one of these data types returns either a true or false value?",
+    A: "String", 
+    B: "Array", 
+    C: "Boolean", 
+    D: "Object"},
+{
+    question: "Inside what HTML element do you add a Javascript file?",
+    A: "<h1>", 
+    B: "<script>", 
+    C: "<div>", 
+    D: "Section"},
+{
+    question: "What is the correct way to write an If statement?",
+    A: "if(i === 5)", 
+    B: "if i = 5 then", 
+    C: "if i === 5", 
+    D: "if i = 5"},
+
+{
+    question: "How do you write a comment in javascript>",
+    A: "<!-- This is a comment -->", 
+    B: "/! This is a comment/!", 
+    C: "`This is a comment`", 
+    D: "// This is a comment"},
+
+{
+    question: "How do you write a comment in javascript>",
+    A: "<!-- This is a comment -->", 
+    B: "/! This is a comment/!", 
+    C: "`This is a comment`", 
+    D: "// This is a comment"},
+
+{
+    question: "Java is a short way of saying javascript?",
+    A: "True", 
+    B: "False"},
+{
+    question: "Which event occurs when the user clicks on an HTML element?",
+    A: "onmouseclick", 
+    B: "onmouseover", 
+    C: "onclick", 
+    D: "onchange"},
+
+{
+    question: "Which operator is used to assign a value to a variable?",
+    A: "-", 
+    B: "*", 
+    C: "X", 
+    D: "="},
+]
